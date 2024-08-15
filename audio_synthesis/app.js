4888c44a72d3b6373cea4140ef97256c5f591d44
Bansker <jud.yannick@protonmail.ch>
2024-08-15 13:13:46 +0200

    added play. you can now play a sine tone

--- audio_synthesis/app.js
+++ audio_synthesis/app.js
@@ -1,10 +1,44 @@
-let context = new AudioContext();
+const btnPlay = document.querySelector('.btn-play');
+const sound = document.querySelector('.sound');
+btnPlay.textContent = 'PLAY';
+
+/* let context = new AudioContext();
 let osc = context.createOscillator();
-let  gain = context.createGain();
+let gain = context.createGain();
 
 osc.type = 'sine';
 osc.frequency.value = 130.8;
 
-osc.connect(gain);
-gain.connect(context.destination);
-osc.start(0);
+osc.start(0); */
+
+btnPlay.addEventListener('click', () => {
+  sound.addEventListener('mouseover', () => gain.connect(context.destination));
+
+  sound.addEventListener('mouseout', () => gain.disconnect(context.destination));
+
+  let context = new AudioContext();
+  let osc = context.createOscillator();
+  osc.type = 'sine';
+  let gain = context.createGain();
+  osc.connect(gain);
+  osc.frequency.value = 261.63; //Play middle C
+  osc.start();
+});
+
+
+
+/* btnPlay.addEventListener('click', (e) =>{
+
+  if(e.target.textContent == 'PLAY') {
+    e.target.textContent = 'STOP';
+    osc.connect(gain);
+    gain.connect(context.destination);
+
+
+  } else {
+    e.target.textContent = 'PLAY';
+    osc.disconnect(gain);
+    gain.disconnect(context.destination);
+
+  }
+}); */
