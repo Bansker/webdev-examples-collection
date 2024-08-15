d4d3fe7fc0b64a6c8ca9c5bdeee22624a313eadc
Bansker <jud.yannick@protonmail.ch>
2024-08-15 15:55:10 +0200

    added stuff to audio_synthesis

--- audio_synthesis/app.js
+++ audio_synthesis/app.js
@@ -1,39 +1,65 @@
-const btnPlay = document.querySelector('.btn-play');
-const sound = document.querySelector('.sound');
-btnPlay.textContent = 'PLAY';
+function l(x) {
+  console.log(x);
+}
 
-/* let context = new AudioContext();
-let osc = context.createOscillator();
-let gain = context.createGain();
-
-osc.type = 'sine';
-osc.frequency.value = 130.8;
-
-osc.start(0); */
+const btnActivate = document.querySelector('.btn-activate');
+const rangeFreq = document.querySelector('.range-freq');
+const outFreq = document.querySelector('.out-freq');
 
 
 
-btnPlay.addEventListener('click', () => {
-  sound.addEventListener('mouseover', () => gain.connect(context.destination));
 
-  sound.addEventListener('mouseout', () => gain.disconnect(context.destination));
+btnActivate.addEventListener('click', (e) => {
+  e.target.textContent = 'Active';
 
   let context = new AudioContext();
   let osc = context.createOscillator();
-  osc.type = 'sine';
   let gain = context.createGain();
+
+  osc.type = 'sine';
   osc.connect(gain);
   osc.frequency.value = 261.63; //Play middle C
+  gain.gain.value = 0;
+  gain.connect(context.destination);
   osc.start();
 
-  // use this instead of connecting / disconnecting to turn off tone (prevents clicking sound)
-  //gain.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 0.04)
 
+
+/*   let lfo = context.createOscillator()
+  let lfoGain = context.createGain()
+
+  lfo.type = 'sine';
+  lfo.connect(lfoGain);
+  lfo.frequency.value = .5;
+  lfoGain.connect(context.destination);
+  lfo.start() */
+
+
+
+
+  document.addEventListener('keydown', (e) => {
+    if(e.key == 'a') {
+      gain.gain.linearRampToValueAtTime(1, context.currentTime + 0.05)
+      l(osc.frequency.value);
+    }
+  });
+  
+  document.addEventListener('keyup', (e) => {
+    if(e.key) {
+      gain.gain.linearRampToValueAtTime(0.00001, context.currentTime + 0.05)
+    }
+  });
+
+  rangeFreq.addEventListener('input', (e) => {
+    outFreq.textContent = e.target.value;
+    osc.frequency.value = e.target.value;
+  });
 });
 
 
 
-/* btnPlay.addEventListener('click', (e) =>{
+
+/* btnActivate.addEventListener('click', (e) =>{
 
   if(e.target.textContent == 'PLAY') {
     e.target.textContent = 'STOP';
