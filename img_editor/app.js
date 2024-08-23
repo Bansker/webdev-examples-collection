086b59a8c664657d9f74d710d4e0787d0ece73f7
Bansker <jud.yannick@protonmail.ch>
2024-08-23 19:08:21 +0200

    Update image editor

--- img_editor/app.js
+++ img_editor/app.js
@@ -3,10 +3,6 @@ function log(x) {
 }
 
 
-
-
-
-
 const imgContainer = document.querySelector('.img-container');
 const imgSubject   = document.querySelector('.img-subject');
 const imgTextbox   = document.querySelector('.img-textbox');
@@ -15,13 +11,15 @@ const imgText      = document.querySelector('.img-text');
 const downloadBtn  = document.querySelector('.button');
 const downloadLink = document.querySelector('.download-link');
 
-const imgTextScale   = 0.1;
+imgContainer.style.height = 800 + 'px';
+
+const imgTextScale     = 0.1;
 const imgTextboxHeight = imgSubject.height * 0.15;
 
 const imgSubjectNaturalHeight = imgSubject.naturalHeight;
 const imgSubjectNaturalWidth  = imgSubject.naturalWidth;
 
-imgContainer.style.height = 500 + 'px';
+//imgContainer.style.height = imgSubjectNaturalHeight + 'px';
 
 imgTextboxStyles = `
   display        : flex;
@@ -45,12 +43,6 @@ imgTextbox.style.cssText = imgTextboxStyles;
 downloadBtn.addEventListener('click', imgDownloader());
 
 function imgDownloader() {
- /* imgContainer.style.height = `${imgSubjectNaturalHeight}px`;
-  let imgTextboxHeight = imgSubject.height * 0.15;
-  imgTextbox.style.height = `${imgTextboxHeight}px`;
-  imgTextbox.style.top = `-${imgTextboxHeight}px`
-  imgTextbox.style.fontSize = `${imgTextboxHeight * 0.24}px`*/
-  
   
   html2canvas(imgContainer, { allowTaint: true }).then((canvas) => {
     downloadLink.download = "mitarbeiterin.jpg";
